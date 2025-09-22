eafa5e7e2c0b508d4bd9907f9f9a4142f42235f0
aitorzubiri <aitorgaillard99@gmail.com>
2025-09-22 13:09:08 +0200

    ariketa9 gui done, falta logica

---
+++ ariketa9/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ariketa9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void salir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(text_box_nuevo.Text))
+            {
+                text_box_lista.Text += text_box_nuevo.Text + Environment.NewLine;
+                text_box_nuevo.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Introduzca datos para poder añadirlos");
+            }
+        }
+    }
+}
